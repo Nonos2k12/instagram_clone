8492c55d4e316cb844dfa787065ef9ba0d785298
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-05-20 17:30:15 +0200

    work on SignUp page + add signup route on routes.js

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -3,10 +3,12 @@ import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import * as ROUTES from '../constants/routes';
 
-export default function Login() {
+export default function SignUp() {
     const history = useHistory();
     const { firebase } = useContext(FirebaseContext);
 
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
 
@@ -16,17 +18,14 @@ export default function Login() {
     const handleSignUp = async (event) => {
         event.preventDefault();
         try {
-            await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-            history.push(ROUTES.DASHBOARD);
+            
         } catch (error) {
-            setEmailAddress('');
-            setPassword('');
-            setError(error.message);
+            
         }
     };
 
     useEffect(() => {
-        document.title = 'Login - Instagram';
+        document.title = 'Sign Up - Instagram';
     }, []);
 
     return (
@@ -45,30 +44,44 @@ export default function Login() {
 
                 <form onSubmit={handleSignUp} method="POST">
                     <input
+                            aria-label="Enter your username"
+                            type="text"
+                            placeholder="Username"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                            onChange={({ target }) => setUsername(target.value)} value={username}
+                        />
+                        <input
+                            aria-label="Enter your full name"
+                            type="text"
+                            placeholder="Full Name"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                            onChange={({ target }) => setFullName(target.value)} value={fullName}
+                        />
+                        <input
                             aria-label="Enter your email address"
                             type="text"
                             placeholder="Email address"
                             className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-                            onChange={({ target }) => setEmailAddress(target.value)}
+                            onChange={({ target }) => setEmailAddress(target.value)} value={emailAddress}
                         />
                         <input
                             aria-label="Enter your password"
                             type="password"
                             placeholder="Password"
                             className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-                            onChange={({ target }) => setPassword(target.value)}
+                            onChange={({ target }) => setPassword(target.value)} value={password}
                         />
                         <button disabled={isInvalid} type="submit" className={`bg-blue-medium text-white w-full rounded h-8 font-bold
                         ${isInvalid && 'opacity-50'}`}
                         >
-                            Log In
+                            Sign up
                         </button>
                 </form>
             </div>
             <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
-                <p className="text-sm">Don't have an account ?{` `}
-                    <Link to="/signup" className="font-bold text-blue-medium">
-                        Sign up
+                <p className="text-sm">Have an account ?{` `}
+                    <Link to="/login" className="font-bold text-blue-medium">
+                        Log In
                     </Link>
                 </p>
             </div>
