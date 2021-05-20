6a17c2a76800f0a5fb0a5ca9c02ce2ac25dcf0b1
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-05-20 19:00:02 +0200

    Finish SignUp page & check for user duplicate in the base + page NotFound

--- src/pages/login.js
+++ src/pages/login.js
@@ -49,14 +49,14 @@ export default function Login() {
                             type="text"
                             placeholder="Email address"
                             className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-                            onChange={({ target }) => setEmailAddress(target.value)}
+                            onChange={({ target }) => setEmailAddress(target.value)} value={emailAddress}
                         />
                         <input
                             aria-label="Enter your password"
                             type="password"
                             placeholder="Password"
                             className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-                            onChange={({ target }) => setPassword(target.value)}
+                            onChange={({ target }) => setPassword(target.value)} value={password}
                         />
                         <button disabled={isInvalid} type="submit" className={`bg-blue-medium text-white w-full rounded h-8 font-bold
                         ${isInvalid && 'opacity-50'}`}
@@ -67,7 +67,7 @@ export default function Login() {
             </div>
             <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
                 <p className="text-sm">Don't have an account ?{` `}
-                    <Link to="/signup" className="font-bold text-blue-medium">
+                    <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
                         Sign up
                     </Link>
                 </p>
