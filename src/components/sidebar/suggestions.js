ae5573910bc16a64d23174c696a9385df1374183
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-05-26 17:07:27 +0200

    Create suggested-profile.js & work on it

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -2,14 +2,15 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
 import { getSuggestedProfiles } from '../../services/firebase';
+import SuggestedProfile from './suggested-profile';
 
-export default function Suggestions({ userId }) {
+export default function Suggestions({ userId, following }) {
     const [profiles, setProfiles] = useState(null);
 
     // Go ahead and get the suggested profiles
     useEffect(() => {
         async function suggestedProfiles() {
-            const response = await getSuggestedProfiles(userId);
+            const response = await getSuggestedProfiles(userId, following);
             setProfiles(response);
         }
 
@@ -30,10 +31,22 @@ export default function Suggestions({ userId }) {
             <div className="text-sm flex items-center align-items justify-between mb-2">
                 <p className="font-bold text-gray-base">Suggestions for you</p>
             </div>
+            <div className="mt-4 grid gap-5">
+                {profiles.map((profile) => (
+                    <SuggestedProfile
+                    key={profile.docId} 
+                    userDocId={profile.docId}
+                    username={profile.username}
+                    profileId={profile.userId}
+                    userId={userId}
+                    />
+                ))}
+            </div>
         </div>
     ) : null;
 }
 
 Suggestions.propTypes = {
-    userId: PropTypes.string
+    userId: PropTypes.string,
+    following: PropTypes.array
 };
