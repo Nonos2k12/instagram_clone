902f156ec1dc1fab74ee88495829ef8cb464e278
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-06-01 17:17:50 +0200

    Continue working on comments, create add-comment.js

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { formatDistance } from 'date-fns';
 import { Link } from 'react-router-dom';
+import AddComment from './add-comment';
 
 export default function Comments({docId, comments: allComments, posted, commentInput}) {
     const [comments, setComments] = useState(allComments);
@@ -23,6 +24,12 @@ export default function Comments({docId, comments: allComments, posted, commentI
             ))}
             <p className="text-gray-base uppercase text-xs mt-2">{formatDistance(posted, new Date())} ago</p>
         </div>
+        <AddComment
+            docId={docId}
+            comments={comments}
+            setComments={setComments}
+            commentInput={commentInput}
+        />
     </>  
     );
 }
