692d873efd004b85b6360086b4fbbd37a19131cf
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-06-03 16:21:53 +0200

    Work on profile's photos component + little modif in tailwind.config.js

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -31,7 +31,6 @@ export default function Profile({ user }) {
                 setFollowerCount ={dispatch}
             />
             <Photos photos={photosCollection} />
-            <p>Hello {user.username}</p>
         </>
     );
 }
