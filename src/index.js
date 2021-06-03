ab20d4878e0992ab80e7ec31274a241e95266266
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-06-03 16:46:26 +0200

    Add header to the not-found page

--- src/index.js
+++ src/index.js
@@ -9,21 +9,5 @@ ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue}}>
     <App />
   </FirebaseContext.Provider>,
-  document.getElementById('root'));
-
-// client side rendered app: react (cra -> create react app)
-  // -> database which is Firebase
-  // -> react_loading-skeleton
-  // tailwind
-
-  // folder structure
-    // src
-      // -> components, 
-      // -> constants, 
-      // -> context, 
-      // -> helpers, 
-      // -> hooks,
-      // -> pages,
-      // -> lib (firebase is going to live in here), 
-      // -> services (firebase functions in here)
-      // -> styles (tailwind's folder (app/tailwind))
+  document.getElementById('root')
+  );
