ae4d83bbaccb0e085997c52eda70c27b21484fd1
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-06-03 15:43:43 +0200

    Add information in profile's header page

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -33,7 +33,7 @@ export default function Header({
     useEffect(() => {
         const isLoggedInUserFollowingProfile = async () => {
             const isFollowing = await isUserFollowingProfile(user.username, profileUserId);
-            setIsFollowingProfile(isFollowing);
+            setIsFollowingProfile(!!isFollowing);
         };
 
         if (user.username && profileUserId) {
@@ -69,6 +69,26 @@ export default function Header({
                             {isFollowingProfile ? 'Unfollow' : 'Follow'}
                         </button>
                         )}
+                </div>
+                <div className="container flex mt-4">
+                        {followers === undefined || following === undefined ? (
+                            <Skeleton count={1} width={677} height={24} />
+                        ) : (
+                          <>
+                            <p className="mr-10">
+                                <span className="font-bold">{photosCount}</span> photos
+                            </p>
+                            <p className="mr-10">
+                                <span className="font-bold">{followerCount}</span>{` `} {followerCount === 1 ? `follower` : `followers`}
+                            </p>
+                            <p className="mr-10">
+                                <span className="font-bold">{following?.length}</span> following
+                            </p>
+                          </>  
+                        )}
+                </div>
+                <div className="container mt-4">
+                        <p className="font-medium">{!fullName ? <Skeleton count={1} height={24} /> : fullName}</p>
                 </div>
             </div>
         </div>
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -143,12 +143,12 @@ export async function toggleFollow(
     profileUserId,
     followingUserId
   ) {
-    // 1st param: karl's doc id
+    // 1st param: Arnaud's doc id
     // 2nd param: raphael's user id
     // 3rd param: is the user following this profile? e.g. does karl follow raphael? (true/false)
     await updateLoggedInUserFollowing(activeUserDocId, profileUserId, isFollowingProfile);
   
-    // 1st param: karl's user id
+    // 1st param: Arnaud's user id
     // 2nd param: raphael's doc id
     // 3rd param: is the user following this profile? e.g. does karl follow raphael? (true/false)
     await updateFollowedUserFollowers(profileDocId, followingUserId, isFollowingProfile);
